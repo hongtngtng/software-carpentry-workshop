76cc498f78e791d2f6286492c1c4977e7d56d1a0
tong <tekawamura@ucsd.edu>
2014-10-21 13:48:24 -0700

    made changes to write parameters

--- analyze_mosquito_data_script.py
+++ analyze_mosquito_data_script.py
@@ -4,7 +4,14 @@ import analyze_mosquito_data_lib as mosquito_lib
 
 filename = 'A1_mosquito_data.csv'
 
+#read the data
 data=pd.read_csv(filename)
 
-print data.head()
+data['temperature']=mosquito_lib.fahr_to_celsius(data['temperature'])
+
+parameters=mosquito_lib.analyze(data)
+
+#writes parameters to file
+parameters.to_csv('parameters.csv')
+
 
