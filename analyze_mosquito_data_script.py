7e34dbb667be0686490c19e18fdd81d2b742c17c
tong <tekawamura@ucsd.edu>
2014-10-21 14:21:39 -0700

    some sys[1] stuff

--- analyze_mosquito_data_script.py
+++ analyze_mosquito_data_script.py
@@ -1,17 +1,23 @@
+import sys
 import pandas as pd
 import analyze_mosquito_data_lib as mosquito_lib
 
 
-filename = 'A1_mosquito_data.csv'
+filename = sys.argv[1]
+
+print "analyzing", filename
 
 #read the data
 data=pd.read_csv(filename)
 
 data['temperature']=mosquito_lib.fahr_to_celsius(data['temperature'])
 
-parameters=mosquito_lib.analyze(data, 'plot.png')
+print "Running Analyze"
+
+parameters=mosquito_lib.analyze(data, filename.replace("csv","png"))
 
 #writes parameters to file
-parameters.to_csv('parameters.csv')
+print "Saving parameters"
+parameters.to_csv(filename.replace("data","parameters"))
 
 
